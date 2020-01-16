f7653c218e2999db21eb02823d8c324cf7ad7b11
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-01-17 00:20:46 +0300

    fix

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def getMyBandsName(login: str):
 def isGoatSecretChat(login: str, secretchat: str):
     goat = getMyGoat(login)
     if goat:
-        if goat['chat']['secret'] == secretchat:
+        if goat['chats']['secret'] == secretchat:
             return True
     else:
         return False
