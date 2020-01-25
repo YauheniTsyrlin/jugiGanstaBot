a2ea8dc30c9ed9f49d9b44731bf2b6320734e1ed
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-01-25 16:02:50 +0300

    add context user

--- dialogflow.py
+++ dialogflow.py
@@ -4,26 +4,58 @@ from google.oauth2 import service_account
 import json
 import config
 import users
+from google.protobuf import struct_pb2
 
-def getResponseDialogFlow(userId: str, text_to_be_analyzed: str, user: user.User):
+def getResponseDialogFlow(session_id: str, text_to_be_analyzed: str, user: users.User):
+
+    contexts = get_contexts(config.DIALOG_FLOW_JSON['project_id'], session_id, "user")
+    if not contexts:
+        print(f'Create context user for {session_id}')
+        parameters = struct_pb2.Struct()
+        parameters['login'] = user.getLogin()
+        parameters['name'] = user.getName()
+        create_context(config.DIALOG_FLOW_JSON['project_id'], session_id, "user", 60, parameters)
 
     credentials = (service_account.Credentials.from_service_account_info(config.DIALOG_FLOW_JSON))
     session_client = dialogflow_v2.SessionsClient(credentials=credentials)
-    session = session_client.session_path(config.DIALOG_FLOW_JSON['project_id'], userId)
+    session = session_client.session_path(config.DIALOG_FLOW_JSON['project_id'], session_id)
 
     text_input = dialogflow_v2.types.TextInput(text=text_to_be_analyzed, language_code='ru-RU')
     query_input = dialogflow_v2.types.QueryInput(text=text_input)
-
     try:
         response = session_client.detect_intent(session=session, query_input=query_input)
+        # print(response.query_result)
     except InvalidArgument:
         raise
-    #print(response.query_result)
-    if user:
-        response.query_result.fulfillment_text = f'{user.getName()}!\n' + response.query_result.fulfillment_text
-    return response.query_result
     
-    # print("Query text:", response.query_result.query_text)
-    # print("Detected intent:", response.query_result.intent.display_name)
-    # print("Detected intent confidence:", response.query_result.intent_detection_confidence)
-    # print("Fulfillment text:", response.query_result.fulfillment_text)
+    return response.query_result
+
+def create_context(project_id, session_id, context_id, lifespan_count, parameters):
+    import dialogflow_v2 as dialogflow
+    contexts_client = dialogflow.ContextsClient(credentials = (service_account.Credentials.from_service_account_info(config.DIALOG_FLOW_JSON)))
+
+    session_path = contexts_client.session_path(project_id, session_id)
+    context_name = contexts_client.context_path(
+        project_id, session_id, context_id)
+
+    context = dialogflow.types.Context(
+        name=context_name, lifespan_count=lifespan_count, parameters=parameters)
+
+    response = contexts_client.create_context(session_path, context)
+
+def get_contexts(project_id, session_id, name):
+    import dialogflow_v2 as dialogflow
+    contexts_client = dialogflow.ContextsClient(credentials = (service_account.Credentials.from_service_account_info(config.DIALOG_FLOW_JSON)))
+    name_context = contexts_client.context_path(project_id, session_id, name)
+    try:
+        context = contexts_client.get_context(name_context)
+        if context:
+            # for field, value in context.parameters.fields.items():
+            #     if value.string_value:
+            #         #print('\t{}: {}'.format(field, value))
+            return context
+        else:
+            print(f'Context {context.name} не найден')
+            return None
+    except:
+        return None
