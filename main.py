dc28b13ca6c83a11cfc766ce3bf4e2f3fce9f1f7
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-02-08 19:50:25 +0300

    fix

--- main.py
+++ main.py
@@ -2654,7 +2654,7 @@ def rade():
             bot.send_sticker(goat['chats']['info'], random.sample(getSetting(code='STICKERS', name='LOVE_DAY'), 1)[0]['value']) 
 
     # Пидор дня
-    if now_date.hour == 19 and now_date.minute == 50 and now_date.second < 15:
+    if now_date.hour == 19 and now_date.minute == 51 and now_date.second < 15:
         updateUser(None)
         user_in_game = []
         for user in USERS_ARR:
