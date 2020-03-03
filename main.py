6e2ef540656c7c8750e6346777b41c2c81b76a35
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-03-03 20:51:26 +0300

    fix

--- main.py
+++ main.py
@@ -881,7 +881,8 @@ def main_message(message):
     userIAm = getUserByLogin(message.from_user.username)
     if userIAm:
         if userIAm.isAccessoryItem(acc_koronavirus):
-            global INFECT_PROBABILITY = float(getSetting(code='PROBABILITY', name='KORONOVIRUS'))
+            global INFECT_PROBABILITY
+            INFECT_PROBABILITY = float(getSetting(code='PROBABILITY', name='KORONOVIRUS'))
         else:
             if INFECT_PROBABILITY > 0.1:
                 may_be_infected = []
