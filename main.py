468e2a1bdcbbdfe568bf594a48f50dc49ed16a92
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-03-09 13:33:34 +0300

    fix

--- main.py
+++ main.py
@@ -3594,7 +3594,7 @@ def rade():
                     send_messages_big(goat['chats']['info'], f'{user.getNameAndGerb()}!\n{getResponseDialogFlow(None, "happy_birthday").fulfillment_text}')
 
     # Присвоение званий
-    if now_date.hour == 10 and now_date.minute == 0 and now_date.second < 15:
+    if now_date.hour == 13 and now_date.minute == 34 and now_date.second < 15:
         report = ''
         updateUser(None)
         for user in USERS_ARR:
@@ -3604,13 +3604,12 @@ def rade():
                 for rank in getSetting(code='RANK', name='MILITARY'):
                     if rank['bm'] > user.getBm():
                         break
-                    else:
-                       newRank = rank
+
                 report = report + f'{rank["bm"]} {rank["name"]} {rank["value"]}\n'
-                if newRank['name'] == user.getRank()['name']:
+                if rank['name'] == user.getRank()['name']:
                     pass
                 else:
-                    user.setRank(newRank)
+                    user.setRank(rank)
                     updateUser(user)
                     goat = getMyGoat(user.getLogin())
                     send_messages_big(goat['chats']['info'], f'{user.getNameAndGerb()}!\n{getResponseDialogFlow(None, "set_new_rank").fulfillment_text}\n{newRank["value"]}')
