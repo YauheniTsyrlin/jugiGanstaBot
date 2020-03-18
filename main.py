de1357b46a29fcc22279dc3c37d20757968b0f7a
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-03-18 19:59:34 +0300

    fix

--- main.py
+++ main.py
@@ -1394,9 +1394,9 @@ def main_message(message):
     elif (message.forward_from and message.forward_from.username == 'WastelandWarsBot' and 'Ты уже записался.' in message.text):
         #write_json(message.json)
         if hasAccessToWariors(message.from_user.username):
-            if message.forward_date < (datetime.now() - timedelta(minutes=5)).timestamp():
-                send_messages_big(message.chat.id, text=getResponseDialogFlow(message, 'deceive').fulfillment_text)
-                return
+            # if message.forward_date < (datetime.now() - timedelta(minutes=5)).timestamp():
+            #     send_messages_big(message.chat.id, text=getResponseDialogFlow(message, 'deceive').fulfillment_text)
+            #     return
 
             u = getUserByLogin(message.from_user.username)
             u.setRaidLocation(1)
