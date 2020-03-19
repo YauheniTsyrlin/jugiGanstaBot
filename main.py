5af1da9038a10932a277764af6ebda64c2696a32
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-03-19 23:08:33 +0300

    fix

--- main.py
+++ main.py
@@ -1402,8 +1402,10 @@ def main_message(message):
             report = ''
             counter_not_find = 0
             for s in strings:
-                logger.info('!!!!!')
                 if start: 
+                    if s.startswith('⚔️'):
+                        continue
+
                     fraction = s.split('(')[1].split(')')[0].strip()
                     pref = ''
                     if '(Без банды)' in s:
@@ -1422,7 +1424,6 @@ def main_message(message):
                         
                 
                 if 'ТОП Купола /tdtop' in s:
-                    logger.info('START!')
                     start = True
             if report == '':
                 send_messages_big(message.chat.id, text='Никого не нашел!')
