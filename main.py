b662500531da5fcb47633be50a9d48e611755a2e
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-03-21 23:04:21 +0300

    fix

--- main.py
+++ main.py
@@ -1878,7 +1878,6 @@ def main_message(message):
                         row.update({'forward_date': forward_date})
 
                 logger.info(row)
-                logger.info(row)
                 newvalues = { "$set": row }
                 result = boss.update_one({
                     'boss_name': name
