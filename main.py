7c45407d194f4df6254610e58d5e6ddd97bc2863
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-03-21 22:24:26 +0300

    fix

--- main.py
+++ main.py
@@ -1798,11 +1798,11 @@ def main_message(message):
                 send_messages_big(message.chat.id, text=getResponseDialogFlow(message, 'no_user').fulfillment_text) 
                 return
 
-            if userIAm.getTimeUpdate() < (datetime.now() - timedelta(days=1000)).timestamp():
+            if userIAm.getTimeUpdate() < (datetime.now() - timedelta(days=1)).timestamp():
                 send_messages_big(message.chat.id, text=getResponseDialogFlow(message, 'update_pip').fulfillment_text) 
                 return
 
-            if message.forward_date < (datetime.now() - timedelta(days=1)).timestamp():
+            if message.forward_date < (datetime.now() - timedelta(days=1000)).timestamp():
                 send_messages_big(message.chat.id, text=getResponseDialogFlow(message, 'old_forward').fulfillment_text) 
                 return
                 
@@ -1889,11 +1889,11 @@ def main_message(message):
                 send_messages_big(message.chat.id, text=getResponseDialogFlow(message, 'no_user').fulfillment_text) 
                 return
 
-            if userIAm.getTimeUpdate() < (datetime.now() - timedelta(days=1000)).timestamp():
+            if userIAm.getTimeUpdate() < (datetime.now() - timedelta(days=1)).timestamp():
                 send_messages_big(message.chat.id, text=getResponseDialogFlow(message, 'update_pip').fulfillment_text) 
                 return
 
-            if message.forward_date < (datetime.now() - timedelta(days=1)).timestamp():
+            if message.forward_date < (datetime.now() - timedelta(days=1000)).timestamp():
                 send_messages_big(message.chat.id, text=getResponseDialogFlow(message, 'old_forward').fulfillment_text) 
                 return
                 
