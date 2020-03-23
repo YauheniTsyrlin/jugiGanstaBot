bc8545b96cdbbf39b62ae9ef5982db67e647f514
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-03-23 20:04:21 +0300

    fix

--- main.py
+++ main.py
@@ -4194,7 +4194,8 @@ def rade():
                     send_messages_big(goat['chats']['info'], f'{user.getNameAndGerb()}!\n{getResponseDialogFlow(None, "happy_birthday").fulfillment_text}')
 
     # Присвоение званий
-    if now_date.hour == 10 and now_date.minute == 5 and now_date.second < 15:
+    if now_date.hour == 20 and now_date.minute == 5 and now_date.second < 15:
+        logger.info('Присвоение званий!')
         report = ''
         updateUser(None)
         for user in USERS_ARR:
