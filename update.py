6d38003b2b8581ebe11a27b5a04a6cf3074eb0d4
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-04-03 23:32:27 +0300

    fix

--- update.py
+++ update.py
@@ -3016,10 +3016,10 @@ print("#==========================#")
 
 
 medic = next((x for i, x in enumerate(getSetting(code='ACCESSORY_ALL', id='SKILLS')['value']) if x['id']=='medic'), None) 
-user = getUserByLogin('Perat_blec')
+user = getUserByLogin('rushkaa')
 # 52 == 31
 # 26 == 13
-medic.update({'storage': 10})
+medic.update({'storage': 25})
 user.removeInventoryThing(medic)
 user.addInventoryThing(medic)
 updateUser(user)
