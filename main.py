19e3d6f311deb6c4230092057c25ec042447e32f
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-04-04 16:59:10 +0300

    fix

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ for setting in settings.find():
     SETTINGS_ARR.append(setting)
 
 GLOBAL_VARS = {
-    'inventory': getSetting(code='ACCESSORY_ALL', id='REWARDS')['value'] + getSetting(code='ACCESSORY_ALL', id='THINGS')['value'] + getSetting(code='ACCESSORY_ALL', id='EDIBLE')['value'] + getSetting(code='ACCESSORY_ALL', id='TATU')['value'] + getSetting(code='ACCESSORY_ALL', id='CLOTHES')['value'] + getSetting(code='ACCESSORY_ALL', id='MARKS_OF_EXCELLENCE')['value'] + getSetting(code='ACCESSORY_ALL', id='POSITIONS')['value']  + getSetting(code='ACCESSORY_ALL', id='POSITIONS')['value'],
+    'inventory': [], #getSetting(code='ACCESSORY_ALL', id='REWARDS')['value'] + getSetting(code='ACCESSORY_ALL', id='THINGS')['value'] + getSetting(code='ACCESSORY_ALL', id='EDIBLE')['value'] + getSetting(code='ACCESSORY_ALL', id='TATU')['value'] + getSetting(code='ACCESSORY_ALL', id='CLOTHES')['value'] + getSetting(code='ACCESSORY_ALL', id='MARKS_OF_EXCELLENCE')['value'] + getSetting(code='ACCESSORY_ALL', id='POSITIONS')['value']  + getSetting(code='ACCESSORY_ALL', id='POSITIONS')['value'],
     'chat_id':
                 {
                     'inventory':[]
