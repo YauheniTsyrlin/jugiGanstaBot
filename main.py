dd8f7074288f62ad8799f5686483ea54dc72562f
Kirill Krivosheev <GonzikBenzyavsky@mail.ru>
2020-05-22 22:48:33 +0300

    fix

--- main.py
+++ main.py
@@ -1606,6 +1606,8 @@ def select_baraholka(call):
                 request = []
 
             cost = inv['cost']
+            request = filter(lambda x : not x['login'] == call.from_user.username, request)
+
             if len(request)>0:
                 cost = max([req['cost'] for req in request])
 
